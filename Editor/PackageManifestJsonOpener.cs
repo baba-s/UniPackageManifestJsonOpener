1d8d3b97773efadb791955886b54d4688fa8b227
baba_s <shotababa1206@gmail.com>
2020-07-10 19:51:04 +0900

    first commit

---
+++ Editor/PackageManifestJsonOpener.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal static class PackageManifestJsonOpener
+	{
+		[MenuItem( "Edit/Open Package manifest.json" )]
+		private static void Open()
+		{
+			var path = $@"{Directory.GetCurrentDirectory()}\Packages\manifest.json";
+			Process.Start( path );
+		}
+	}
+}
