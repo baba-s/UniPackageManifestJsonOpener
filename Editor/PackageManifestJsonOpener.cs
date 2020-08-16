00bf3739bb30ba1a8629eb7641b983ed1ad0f945
baba_s <shotababa1206@gmail.com>
2020-08-16 20:58:14 +0900

    add menu item

--- Editor/PackageManifestJsonOpener.cs
+++ Editor/PackageManifestJsonOpener.cs
@@ -6,10 +6,17 @@ namespace Kogane.Internal
 {
 	internal static class PackageManifestJsonOpener
 	{
-		[MenuItem( "Edit/Open Package manifest.json" )]
-		private static void Open()
+		[MenuItem( "Edit/UniPackageManifestJsonOpener/Open manifest.json" )]
+		private static void OpenManifestJson()
 		{
 			var path = $@"{Directory.GetCurrentDirectory()}\Packages\manifest.json";
+			Process.Start( path );
+		}
+
+		[MenuItem( "Edit/UniPackageManifestJsonOpener/Open packages-lock.json" )]
+		private static void OpenPackagesLockJson()
+		{
+			var path = $@"{Directory.GetCurrentDirectory()}\Packages\packages-lock.json";
 			Process.Start( path );
 		}
 	}
